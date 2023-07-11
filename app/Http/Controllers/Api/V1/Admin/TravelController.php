b6ab0847388588f088094e6a6ea92cd0804ea499
Edzel <dev.edzeldacanay@gmail.com>
2023-07-11 16:05:57 +0800

    updated

--- app/Http/Controllers/Api/V1/Admin/TravelController.php
+++ app/Http/Controllers/Api/V1/Admin/TravelController.php
@@ -11,7 +11,7 @@ class TravelController extends Controller
 {
 
     public function store(TravelRequest $request)
-    {
+    {   
         $travel = Travel::create($request->validated());
 
         return new TravelResource($travel);
@@ -24,4 +24,6 @@ class TravelController extends Controller
 
         return new TravelResource($travel);
     }
+
+    
 }
